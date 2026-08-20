d65249e5341da07b27b8a69f249e338f997f6c1d
Hello-Aftab <aftabali62029@gmail.com>
2026-08-20 13:09:26 +0530

    Initial commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 name = st.text_input("Enter Your Name")
 fname = st.text_input("Enter Your Father Name : ")
+fname = st.text_input("Enter Your Mother Name : ")
 adr = st.text_area("Enter Your Text : ")
 classdata = st.selectbox("Enter Your Class :",(1,2,3,4,5,6))
 
@@ -12,5 +13,6 @@ if button :
     st.markdown(f"""
     Name : {name}
     Father Name : {fname}
+    Mother Name : {fname}
     address : {adr}
     class : {classdata}""")
